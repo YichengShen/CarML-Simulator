cccc2a9cb5bfbe38bad0e1a1ab3284e2de2d8988
Alexduanran <alexduanran@gmail.com>
2020-06-15 00:18:14 -0400

    Neural Network SGD V1

---
+++ simulation_SGD_NN/simulationClass.py
@@ -0,0 +1,319 @@
+import math
+import heapq
+import numpy as np
+import yaml
+import xml.etree.ElementTree as ET 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+file = open('config.yml', 'r')
+cfg = yaml.load(file, Loader=yaml.FullLoader)
+
+np.random.seed(3)
+
+class Vehicle:
+    """
+    Vehicle object for Car ML Simulator.
+    Attributes:
+    - car_id
+    - x
+    - y
+    - speed
+    - comp_power
+    - bandwidth
+    - rsu_assigned
+    - model
+    - training_data_assigned
+    - num_training_data-downloaded
+    - training_label_assigned
+    - num_training_label_downloaded
+    - gradients
+    - upload_complete
+    - lock
+    """
+    def __init__(self, car_id, comp_power, comp_power_std, bandwidth, bandwidth_std):
+        self.car_id = car_id
+        self.x = 0
+        self.y = 0
+        self.speed = 0
+        self.comp_power = np.random.normal(comp_power, comp_power_std)
+        self.bandwidth = np.random.normal(bandwidth, bandwidth_std)
+        self.rsu_assigned = None
+        self.model = None
+        self.training_data_assigned = []
+        self.num_training_data_downloaded = 0
+        self.training_label_assigned = []
+        self.num_training_label_downloaded = 0
+        self.gradients = None
+        self.upload_complete = False
+        self.lock = 0
+
+    def set_properties(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    # Return the RSU that is cloest to the vehicle
+    def closest_rsu(self, rsu_list):
+        shortest_distance = 99999999 # placeholder (a random large number)
+        closest_rsu = None
+        for rsu in rsu_list:
+            distance = math.sqrt((rsu.rsu_x - self.x) ** 2 + (rsu.rsu_y - self.y) ** 2)
+            if distance <= rsu.rsu_range and distance < shortest_distance:
+                shortest_distance = distance
+                closest_rsu = rsu
+        return closest_rsu
+
+    # Return a list of RSUs that is within the range of the vehicle
+    # with the first element of the list being the RSU that is cloest to the vehicle
+    def in_range_rsus(self, rsu_list):
+        in_range_rsus = []
+        for rsu in rsu_list:
+            distance = math.sqrt((rsu.rsu_x - self.x) ** 2 + (rsu.rsu_y - self.y) ** 2)
+            if distance <= rsu.rsu_range:
+                heapq.heappush(in_range_rsus, (distance, rsu))
+        return list(map(lambda x: x[1], in_range_rsus))
+
+    def download_from_rsu(self, rsu_list):
+        if self.rsu_assigned is None:
+            for rsu in rsu_list:
+                if rsu.dataset:
+                    self.rsu_assigned = rsu
+                    rsu.vehicle_traffic += 1
+                    dataset = rsu.dataset.pop()
+                    self.training_data_assigned = dataset[0]
+                    self.training_label_assigned = dataset[1]
+                    break
+        self.num_training_data_downloaded  += int(self.bandwidth)
+        self.num_training_label_downloaded += int(self.bandwidth)
+
+    def download_from_central_server(self):
+        self.num_training_data_downloaded  += int(self.bandwidth / 4)
+        self.num_training_label_downloaded += int(self.bandwidth / 4)
+        
+    def download_completed(self):
+        if self.rsu_assigned is not None:
+            return self.num_training_data_downloaded >= len(self.training_data_assigned)
+        return False
+
+    def download_model_from(self, central_server):
+        self.model = central_server.model
+
+    # Assume the car downloads the model directly from the central server when it is computing
+    # After completing, store the gradients
+    def compute_async(self, central_server):
+        neural_network = Neural_Network()
+        loss_value, self.gradients = neural_network.grad(central_server.model, np.array(self.training_data_assigned), np.array(self.training_label_assigned))
+        central_server.epoch_loss_avg.update_state(loss_value)
+        lock_time = int(len(self.training_data_assigned) / self.comp_power) - 1
+        self.lock += lock_time
+
+    def compute_completed(self):
+        return self.gradients is not None
+
+    # Assume the car directly uploads the gradients to the central server and takes zero time
+    def upload_gradients_async(self, central_server):
+        neural_network = Neural_Network()
+        neural_network.optimizer.apply_gradients(zip(self.gradients, central_server.model.trainable_variables))
+        central_server.epoch_accuracy.update_state(np.array(self.training_label_assigned), central_server.model(np.array(self.training_data_assigned), training=True))
+        central_server.gradients_received += 1
+        self.upload_complete = True
+
+    def upload_completed(self):
+        return self.upload_complete
+
+    def locked(self):
+        return self.lock > 0
+
+    def update_lock(self):
+        self.lock -= 1
+
+    def free_up(self):
+        self.training_data_assigned = []
+        self.num_training_data_downloaded = 0
+        self.training_label_assigned = []
+        self.num_training_label_downloaded = 0
+        self.rsu_assigned = None
+        self.gradients = None
+        self.upload_complete = False
+        
+
+class RSU:
+    """
+    Road Side Unit object for Car ML Simulator.
+    Attributes:
+    - rsu_id
+    - rsu_x
+    - rsu_y
+    - rsu_range
+    - dataset
+    - vehicle_traffic
+    - traffic_proportion
+    """
+    def __init__(self, rsu_id, rsu_x, rsu_y, rsu_range):
+        self.rsu_id = rsu_id
+        self.rsu_x = rsu_x
+        self.rsu_y = rsu_y
+        self.rsu_range = rsu_range
+        self.dataset = []
+        self.vehicle_traffic = 0
+        self.traffic_proportion = 1 / cfg['simulation']['num_rsu']
+
+    def low_on_data(self):
+        return len(self.dataset) < 10
+
+
+class Central_Server:
+    """
+    Central Server object for Car ML Simulator.
+    Attributes:
+    - dataset
+    - train_dataset
+    - test_dataset
+    - model
+    - epoch_loss_avg
+    - epoch_accuracy
+    - num_epoch
+    - rsu_list
+    - num_distributed
+    - gradients_received
+    """
+    def __init__(self, rsu_list):
+        train, test = tf.keras.datasets.mnist.load_data()
+
+        # Normalize the training data to fit the model
+        train_images, train_labels = train
+        train_images, train_labels = train_images[:10000], train_labels[:10000]
+        train_images = train_images.reshape(train_images.shape[0], 784)
+        train_images = train_images/255
+
+        # Normalize the testing data to fit the model
+        test_images, test_labels = test
+        test_images, test_labels = test_images[:10000], test_labels[:10000]
+        test_images = test_images.reshape(test_images.shape[0], 784)
+        test_images = test_images/255
+
+        self.dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(200).batch(cfg['neural_network']['batch_size'])
+        self.train_dataset = []
+        self.test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(cfg['neural_network']['batch_size'])
+        # The structure of the neural network
+        self.model = tf.keras.Sequential([
+                    tf.keras.layers.Dense(128, activation=tf.nn.relu, input_shape=(784,)),  # input shape required
+                    # tf.keras.layers.Dense(10, activation=tf.nn.relu),
+                    tf.keras.layers.Dense(10)
+        ])
+        self.epoch_loss_avg = tf.keras.metrics.Mean()
+        self.epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+        self.num_epoch = 0
+        self.rsu_list = rsu_list
+        self.num_distributed = 0        # Amount of mini-batches already distributed out to RSUs
+        self.gradients_received = 0     # Number of gradients received from vehicle in each epoch (Async)
+
+    # Initially distribute 1/4 of the data to each RSU
+    def distribute_to_rsu(self, degree_of_overlap = 0):
+        num_mini_batches = len(self.train_dataset)
+        for rsu in self.rsu_list:
+            initial_distribution = int(0.25 * num_mini_batches * rsu.traffic_proportion)
+            initial_overlapping = int(degree_of_overlap * num_mini_batches)
+            total_distribution = initial_distribution + initial_overlapping
+            rsu.dataset.extend(self.train_dataset[self.num_distributed:total_distribution])
+            self.num_distributed += initial_distribution
+    
+    # Redistribute part of the data to RSU when the RSU is running low on data
+    def redistribute_to_rsu(self, rsu):
+        num_redistributed = int(0.25 * len(self.train_dataset) * rsu.traffic_proportion)
+        rsu.dataset.extend(self.train_dataset[self.num_distributed:self.num_distributed + num_redistributed])
+        self.num_distributed += num_redistributed
+
+    def epoch_completed(self):
+        # return self.gradients_received == len(self.train_dataset) / cfg['neural_network']['batch_size']
+        return self.num_distributed >= len(self.train_dataset)
+
+    def print_accuracy(self):
+        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(self.num_epoch,
+                                                                self.epoch_loss_avg.result(),
+                                                                self.epoch_accuracy.result()))
+
+    def new_epoch(self):
+        # Need to loop through the dataset each epoch to shuffle the data
+        for x in self.dataset:
+            self.train_dataset.append(x)
+        self.num_epoch += 1
+        self.distribute_to_rsu()
+        self.num_distributed = 0
+        self.gradients_received = 0
+
+
+class Neural_Network:
+    """
+    Neural network functions
+    Attributes:
+    - optimizer
+    """
+    def __init__(self):
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+
+    # The loss function
+    def loss(self, model, x, y, training):
+        loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        y_ = model(x, training=training)
+        return loss_object(y_true=y, y_pred=y_)
+    
+    # Gradients and loss
+    def grad(self, model, inputs, targets):
+        with tf.GradientTape() as tape:
+            loss_value = self.loss(model, inputs, targets, training=True)
+        return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+
+class SUMO_Dataset:
+    """
+    Data read from SUMO XML files.
+    Attributes:
+    - ROU_file
+    - NET_file
+    """
+    def __init__(self, ROU_file, NET_file):
+        self.ROU_file = ROU_file
+        self.NET_file = NET_file
+
+    def vehicleDict(self, comp_power, comp_power_std, bandwidth, bandwidth_std):
+        tree = ET.parse(self.ROU_file)
+        root = tree.getroot()
+        vehicle_dict = {}
+        for vehicle in root.findall('trip'):
+            vehicle_dict[vehicle.attrib['id']] = Vehicle(vehicle.attrib['id'], comp_power, comp_power_std, bandwidth, bandwidth_std)
+        return vehicle_dict
+
+    def rsuList(self, rsu_range, rsu_nums):
+        tree = ET.parse(self.NET_file)
+        root = tree.getroot()
+        rsu_list = []
+        junction_list = np.random.choice(root.findall('junction'), rsu_nums, replace=False)
+        for i in range(rsu_nums):
+            id = 'rsu' + str(i)
+            rsu_list.append(RSU(id, float(junction_list[i].attrib['x']), float(junction_list[i].attrib['y']), rsu_range))
+        return rsu_list
+
+class Simulation:
+    """
+    Simulation object for Car ML Simulator.
+    Attributes:
+    - FCD_file
+    - vehicle_dict
+    - rsu_list
+    - dataset
+    """
+    def __init__(self, FCD_file, vehicle_dict: dict, rsu_list: list, central_server):
+        self.FCD_file = FCD_file
+        self.vehicle_dict = vehicle_dict
+        self.rsu_list = rsu_list
+        self.central_server = central_server
+       
+    def add_into_vehicle_dict(self, vehicle):
+        self.vehicle_dict[vehicle.attrib['id']] = Vehicle(vehicle.attrib['id'],
+                                                          cfg['simulation']['comp_power'],
+                                                          cfg['simulation']['comp_power_std'], 
+                                                          cfg['simulation']['bandwidth'],
+                                                          cfg['simulation']['bandwidth_std'])
---
+++ simulation_SGD_NN/simulationItr.py
@@ -0,0 +1,98 @@
+from simulationClass import *
+import xml.etree.ElementTree as ET 
+
+def simulate(simulation):
+    simulation.central_server.new_epoch()
+
+    tree = ET.parse(simulation.FCD_file)
+    root = tree.getroot()
+
+    # For each time step (sec) in the FCD file 
+    for timestep in root:
+        # Maximum training epochs
+        if simulation.central_server.num_epoch <= cfg['neural_network']['epoch']:
+
+            # Calculate in-real-time RSU traffic every 10 minutes
+            if timestep.attrib['time'] != '0.00' and float(timestep.attrib['time']) % 600 == 0:
+                total_traffic = sum(map(lambda x: x.vehicle_traffic, simulation.rsu_list))
+                for rsu in simulation.rsu_list:
+                    rsu.traffic_proportion = rsu.vehicle_traffic / total_traffic
+                    rsu.vehicle_traffic = 0
+
+            # Redistribute data from central server to RSU if RSU is about to run out of data
+            for rsu in simulation.rsu_list:
+                if rsu.low_on_data():
+                    simulation.central_server.redistribute_to_rsu(rsu)
+
+            # When each epoch is completed, print accuracy of each epoch
+            if simulation.central_server.epoch_completed():
+                simulation.central_server.print_accuracy()
+                simulation.central_server.new_epoch()
+
+            # For each vehicle on the map at the timestep
+            for vehicle in timestep.findall('vehicle'):
+                # If vehicle not yet stored in vehicle_dict
+                if vehicle.attrib['id'] not in simulation.vehicle_dict:
+                    simulation.add_into_vehicle_dict(vehicle)
+                # Get the vehicle object from vehicle_dict
+                vehi = simulation.vehicle_dict[vehicle.attrib['id']]  
+                # Set location and speed
+                vehi.set_properties(float(vehicle.attrib['x']),
+                                    float(vehicle.attrib['y']),
+                                    float(vehicle.attrib['speed']))
+                # Download if not finished downloading
+                if not vehi.download_completed():
+                    rsus_in_range = vehi.in_range_rsus(simulation.rsu_list)
+                    # Download from RSU if in range of RSU
+                    if vehi.rsu_assigned is None or vehi.rsu_assigned in rsus_in_range:
+                        vehi.download_from_rsu(rsus_in_range)
+                    # Download from the central server if not in range of RSU
+                    else:
+                        vehi.download_from_central_server()
+                # If finished downloading
+                else:
+                    # If the vehicle is still computing
+                    if not vehi.locked():
+                        # Compute the gradients using the latest model
+                        if not vehi.compute_completed():
+                            vehi.compute_async(simulation.central_server)
+                        # If finished computing
+                        else:
+                            # Upload the gradients if not finished uploading
+                            if not vehi.upload_completed():
+                                vehi.upload_gradients_async(simulation.central_server)
+                            else:
+                                vehi.free_up()
+                    # If locked, lock -1 in every time step
+                    else:
+                        vehi.update_lock()
+    return simulation.central_server.model
+
+            
+def main():
+    ROU_FILE = cfg['simulation']['ROU_FILE']
+    NET_FILE = cfg['simulation']['NET_FILE']
+    FCD_FILE = cfg['simulation']['FCD_FILE']
+    
+    RSU_RANGE = cfg['comm_range']['v2rsu']           # range of RSU
+    NUM_RSU = cfg['simulation']['num_rsu']           # number of RSU
+
+    sumo_data = SUMO_Dataset(ROU_FILE, NET_FILE)
+    vehicle_dict = {}
+    rsu_list = sumo_data.rsuList(RSU_RANGE, NUM_RSU)
+    central_server = Central_Server(rsu_list)
+
+    simulation = Simulation(FCD_FILE, vehicle_dict, rsu_list, central_server)
+    model = simulate(simulation)
+
+    # Test the accuracy of the computed model
+    test_accuracy = tf.keras.metrics.Accuracy()
+    for (x, y) in central_server.test_dataset:
+        logits = model(x, training=False)
+        prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
+        test_accuracy(prediction, y)
+    print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+
+
+if __name__ == '__main__':
+    main()
